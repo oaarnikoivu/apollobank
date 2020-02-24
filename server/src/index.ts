0ffc9853352c741978cc30306fcce1389ef1c2c4
oaarnikoivu <oliver.aarnikoivu@outlook.com>
2020-02-24 10:26:09 +0000

    install security dependencies

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,22 @@
-import { Express, Response, Request } from 'express';
-import express from 'express';
+import express, { Express, Response, Request } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+
+require('dotenv').config();
 
 const app: Express = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
+
+app.use(morgan('common'));
+app.use(helmet());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  }),
+);
+app.use(express.json());
 
 app.get('/', (_req: Request, res: Response) => {
   res.json({
