0d7b6aadc3417beb2c66260a38c3b8e4cee5f22b
oaarnikoivu <oliver.aarnikoivu@outlook.com>
2020-02-24 10:55:13 +0000

    config more routes

--- server/src/api/routes.ts
+++ server/src/api/routes.ts
@@ -8,8 +8,32 @@ router.get('/', (_req: Request, res: Response) => {
   });
 });
 
-router.get('/account', (_req: Request, res: Response) => {
+router.get('/accounts', (_req: Request, res: Response) => {
   res.json({
-    message: 'Account Page',
+    message: 'Accounts Page',
+  });
+});
+
+router.get('/analytics', (_req: Request, res: Response) => {
+  res.json({
+    message: 'Analytics Page',
+  });
+});
+
+router.get('/payments', (_req: Request, res: Response) => {
+  res.json({
+    message: 'Payments Page',
+  });
+});
+
+router.get('/cards', (_req: Request, res: Response) => {
+  res.json({
+    message: 'Cards Page',
+  });
+});
+
+router.get('/dashboard', (_req: Request, res: Response) => {
+  res.json({
+    message: 'Dashboard Page',
   });
 });
