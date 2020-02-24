302a17e6c828f7e9a6a0a35c266deb80554154d8
oaarnikoivu <oliver.aarnikoivu@outlook.com>
2020-02-24 19:40:54 +0000

    refactor dummy data

--- server/src/api/dummyData.ts
+++ server/src/api/dummyData.ts
@@ -1,4 +1,4 @@
-export interface User {
+interface User {
   id: string;
   email: string;
   password: string;
@@ -12,8 +12,36 @@ export interface User {
   country: string;
 }
 
-export interface Account {
+interface Account {
   owner: User;
   currency: string;
   balance: number;
 }
+
+const dummyUser: User = {
+  id: '1',
+  email: 'oliver.aarnikoivu@outlook.com',
+  password: 'password',
+  firstName: 'Oliver',
+  lastName: 'Aarnikoivu',
+  dateOfBirth: new Date(1996, 9, 29),
+  phone: '+44 (0) 7599 469798',
+  streetAddress: '45 An Der Merzel',
+  postCode: '8350',
+  city: 'Garnich',
+  country: 'Luxembourg',
+};
+
+const dummyAccount01: Account = {
+  owner: dummyUser,
+  currency: 'EUR',
+  balance: 7000,
+};
+
+const dummyAccount02: Account = {
+  owner: dummyUser,
+  currency: 'GBP',
+  balance: 60,
+};
+
+export const accounts: Account[] = [dummyAccount01, dummyAccount02];
--- server/src/api/routes.ts
+++ server/src/api/routes.ts
@@ -1,35 +1,7 @@
 import { Router, Response, Request } from 'express';
-import { Account, User } from './dummyData';
+import { accounts } from './dummyData';
 
 export const router: Router = Router();
-
-const dummyUser: User = {
-  id: '1',
-  email: 'oliver.aarnikoivu@outlook.com',
-  password: 'password',
-  firstName: 'Oliver',
-  lastName: 'Aarnikoivu',
-  dateOfBirth: new Date(1996, 9, 29),
-  phone: '+44 (0) 7599 469798',
-  streetAddress: '45 An Der Merzel',
-  postCode: '8350',
-  city: 'Garnich',
-  country: 'Luxembourg',
-};
-
-const dummyAccount01: Account = {
-  owner: dummyUser,
-  currency: 'EUR',
-  balance: 7000,
-};
-
-const dummyAccount02: Account = {
-  owner: dummyUser,
-  currency: 'GBP',
-  balance: 60,
-};
-
-const accounts: Account[] = [dummyAccount01, dummyAccount02];
 
 router.get('/', (_req: Request, res: Response) => {
   res.json({
