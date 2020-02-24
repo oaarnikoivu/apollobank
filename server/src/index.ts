418ca4f39a3f94fee26c16bb2650785e992d5411
oaarnikoivu <oliver.aarnikoivu@outlook.com>
2020-02-24 10:17:52 +0000

    init server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
+import { Express, Response, Request } from 'express';
+import express from 'express';
 
+const app: Express = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.json({
+    message: 'Hello World!',
+  });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
