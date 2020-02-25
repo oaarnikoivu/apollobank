bfe9d8f26578a50abf7fb8a744094d186d7e7934
oaarnikoivu <oliver.aarnikoivu@outlook.com>
2020-02-25 17:32:30 +0000

    testing validation schemas using Joi

--- server/src/api/routes.ts
+++ server/src/api/routes.ts
@@ -1,7 +1,36 @@
 import { Router, Response, Request } from 'express';
 import { authRouter } from './auth/authRoutes';
+import Joi from 'joi';
 
 export const router: Router = Router();
+
+const schema: Joi.ObjectSchema = Joi.object().keys({
+  email: Joi.string()
+    .email()
+    .required(),
+  password: Joi.string()
+    .min(6)
+    .required(),
+  firstName: Joi.string()
+    .regex(/^[a-zA-Z]+$/)
+    .required(),
+  lastName: Joi.string()
+    .regex(/^[a-zA-Z]+$/)
+    .required(),
+  dateOfBirth: Joi.date().required(),
+  phone: Joi.string()
+    .trim()
+    .regex(/^[0-9]{7,10}$/)
+    .required(),
+  streetAddress: Joi.string()
+    .trim()
+    .regex(/^[a-z\d\s\-\.\,]*$/i)
+    .max(100)
+    .required(),
+  postCode: Joi.string().required(),
+  city: Joi.string().required(),
+  country: Joi.string().required(),
+});
 
 router.get('/', (_req: Request, res: Response) => {
   res.json({
@@ -9,11 +38,10 @@ router.get('/', (_req: Request, res: Response) => {
   });
 });
 
-router.post('/signup', (_req: Request, res: Response) => {
-  res.json({
-    message: 'Signup route',
-  });
+// routes which require authentication
+router.post('/signup', (req: Request, res: Response) => {
+  const result: Joi.ValidationResult<any> = Joi.validate(req.body, schema);
+  res.json(result);
 });
 
-// routes which require authentication
 router.use('/api', authRouter);
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,5 +1,4 @@
-import mongoose from 'mongoose';
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
 
 export interface User extends Document {
   email: string;
