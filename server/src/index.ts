addfada1967062720dff071a39c3ac9887739979
oaarnikoivu <oliver.aarnikoivu@outlook.com>
2020-03-07 15:56:16 +0000

    try again...

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ import { createTypeOrmConnection } from "./utils/createTypeOrmConnection";
 	app.use(cookieParser());
 	app.use(
 		cors({
-			origin: process.env.FRONTEND_HOST as string,
+			origin: "https://vigilant-goldwasser-9ac664.netlify.com/",
 			credentials: true
 		})
 	);
